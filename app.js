49ca45987f589d9689ad1b306315816e05e0cf80
saurabhsonje <50398982+saurabhsonje@users.noreply.github.com>
2025-06-15 17:13:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-
+exports.handler = async (event) => {
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'text/plain',
+    },
+    body: 'Hello from Lambda containerized Node.js from reusable workflow\n',
+  };
+};
